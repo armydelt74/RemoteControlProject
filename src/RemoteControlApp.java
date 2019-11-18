ea467ba52fec084b83a28459b7bb67c5ec07d1d4
armydelt74 <kgneiting@gmail.com>
2019-11-17 18:22:04 -0700

    Revised Controll app - still working last method

--- src/RemoteControlApp.java
+++ src/RemoteControlApp.java
@@ -1,130 +1,93 @@
+import java.util.Scanner;
 
 public class RemoteControlApp {
 
+	Scanner kb = new Scanner(System.in);
+
 	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		printMenu();
+		boolean on = true; //Used boolean for True/False until caps "ON" condition met
+		while (on == true) {
 
-				printMenu();
-				java.util.Scanner kb = new java.util.Scanner(System.in);
-			    	
-				System.out.println("This is the Remote Control 5000... a glowing ON button calls to you...");
-				String turnon = kb.next();
-				
-				if (turnon.equals("ON") || turnon.equals("On") || turnon.equals("on")) {
-					System.out.println("Welcome to JAVA TV+ - where every channel takes a part of your soul");
-				}
-				
-				else {
-					System.err.println("You need to turn me on... cough - try again");
-				}
-					
-				String choice = "";
-				do {
-					choice = kb.next();
-					switch (choice) {
-					
-					case "1":
-						printButtonboop();
-						break;
+			System.out.println("TO BEGIN TURN ON THE REMOTE CONTROL 5000");
 
-					case "2":
-						printButtonboop();
-						break;
+			String turnon = kb.next();
 
-					case "3":
-						printButtonboop();
-						break;
-
-					case "4":
-						printButtonboop();
-						break;
-
-					case "5":
-						printButtonboop();
-						break;
-
-					case "6":
-						printButtonbeep();
-						break;
-						
-					case "7":
-						printButtonbeep();
-						break;
-						
-					case "8":
-						printButtonbeep();
-						break;
-						
-					case "9":
-						printButtonbeep();
-						break;
-						
-					case "0":
-						printButtonbeep();
-						break;
-
-					case "00":
-						printButtontime();
-						break;
-					
-					case "Off":
-						printOff();
-						break;
-
-					case "off":
-						printOff();
-						break;
-					}
-
-				} while (!choice.equalsIgnoreCase("Off"));
-				
-//				int counter = 0; // thought make counter variable requires 2 digit input on remote
-			  
-//				while (counter < 2)
-//			    	counter = counter +1;
-			    
-				kb.close();
-				
-				
-
+			if (turnon.equals("ON")) {
+				on = false;
+				System.out.println("TURNING ON");
+				greetIn();
 			}
 
-			public static void printMenu() {
-				System.out.println("?????????????????");
-				System.out.println("|***** ON ******|");
-				System.out.println("|*1* *2* *3* *4*|");
-				System.out.println("|***************|");
-				System.out.println("|*5* *6* *7* *8*|");
-				System.out.println("|***************|");
-				System.out.println("| *9* *0* *00*  |");
-				System.out.println("&&&&&&&&&&&&&&&&&");
+		}
+		powerOn();
+		kb.close();
+	}
 
-			}
+	public static void powerOn() {
+		Scanner kb = new Scanner(System.in);
+		String choice = "";
+		choice = kb.next().toLowerCase();
+		switch (choice) {
 
-			public static void printOn() {
-				System.out.println("Welcome To Java TV+");
-			}
-
-			public static void printButtonboop() {
-				System.out.println("BOOOOP");
-			}
-
-			public static void printButtonbeep() {
-				System.out.println("BEEEEP");
-			}
-
-			public static void printOff() {
-				System.out.println("You have enrolled in Java TV+ autobill for $59 per month");
-			}
-
-			public static void printButtontime() {
-				System.out.println("You have entered an infinite loop");
-			}
-
-		
+		case "1":
+		case "2":
+		case "3":
+		case "4":
+		case "5":
+		case "6":
+		case "7":
+		case "8":
+		case "9":
+		case "0":
+		case "!off":
+			printButtonboop(); // Case 0-9 call printButton method (fall through method)
+			break;
 			
+		case "off":
+			printOff();
+			break;
+
+		}
+//		while (!choice.equalsIgnoreCase("Off")) {
+
+		kb.close();
+	}
+
+	public static void printMenu() {
+		System.out.println("?????????????????");
+		System.out.println("|***** ON ******|");
+		System.out.println("|*1* *2* *3* *4*|");
+		System.out.println("|***************|");
+		System.out.println("|*5* *6* *7* *8*|");
+		System.out.println("|***************|");
+		System.out.println("| *9* *0* *00*  |");
+		System.out.println("&&&&&&&&&&&&&&&&&");
+
+	}
+
+	public static void greetIn() {
+		System.out.println(
+				"Welcome to JAVA TV+ - where every channel takes a part of your soul, please select your channel");
+	}
+
+	public static void printButtonboop() {
+		System.out.println("BOOOOP");
+		System.out.println("Please enter 0-9");
+		powerOn();
+
+	}
+	
+	public static void printWrong() {
+		System.out.println("Command not recognized");
+	}
+
+	public static void printOff() {
+		System.out.println("You have enrolled in Java TV+ autobill for $59 per month, Goodbye");
+	}
 
 }
-
-// TODO: ADD pressButton() method
-// TODO: Add turnOn() method
-
+//
+//// TODO: ADD pressButton() method
+//// TODO: Add turnOn() method
